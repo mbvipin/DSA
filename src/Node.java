2f1ecef07e0be2833e72c79063671d00a1e652df
mbvipin <mbvipin@gmail.com>
2022-05-07 20:39:26 -0400

    Node class added

---
+++ src/Node.java
@@ -0,0 +1,27 @@
+public class Node {
+
+    Node next;
+
+    Node(int data)
+    {
+        this.data=data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    int data;
+}
